254bc0401ad61a3840ce629ae83c1eb7dc29f0fa
Brian Lund Lauridsen <brian@setsnail.com>
2016-10-12 21:52:50 +0200

    Sync

---
+++ Assets/ColorButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class ColorButton : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerUpHandler {
+	public Image colorFill;
+	public BacteriaElement element;
+
+	public Bacteria bacteriaPrefab;
+
+	private Bacteria _instantiatedBacteria;
+	private bool _leftArea;
+
+	public void OnPointerDown(PointerEventData data ) {
+		_instantiatedBacteria = Instantiate( bacteriaPrefab, transform.position, Quaternion.identity) as Bacteria;
+
+		_leftArea = false;
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		_leftArea = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData) {
+		if(!_leftArea) {
+			Destroy(_instantiatedBacteria);
+		}
+	}
+
+	public void OnValidate() {
+		if(colorFill != null) {
+			colorFill.color = element.color;
+		}
+	}
+}
--- Assets/Script/Bacteria.cs
+++ Assets/Script/Bacteria.cs
@@ -5,23 +5,14 @@ public class Bacteria : MonoBehaviour {
 	public BacteriaElement bacteriaType;
 	public UnitType unitType;
 
-	public float damage;
-	public float health;
-	public float speed;
-
-	public int cost;
+	float _damage;
+	float _health;
+	float _speed;
 
 	private SpriteRenderer _renderer;
 
 	public void Start() {
-		_renderer = GetComponent<SpriteRenderer>();
-
-		_renderer.color = bacteriaType.color;
-	}
-
-	void OnValidate() {
-		if(bacteriaType != null) {
-			GetComponent<SpriteRenderer>().color = bacteriaType.color;
-		}
+		//_renderer = GetComponent<SpriteRenderer>();
+		//_renderer.color = bacteriaType.color;
 	}
 }
